2bf869edceabc9f396d82fe19ba9440f06649eeb
Arunesh Gupta <arunesh.gupta@globallogic.com>
2023-07-11 15:54:25 +0530

    Added SDK xc.Framework

---
+++ POC_Cocoapods/POC_Cocoapods/POC_Cocoapods.h
@@ -0,0 +1,18 @@
+//
+//  POC_Cocoapods.h
+//  POC_Cocoapods
+//
+//  Created by global on 11/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for POC_Cocoapods.
+FOUNDATION_EXPORT double POC_CocoapodsVersionNumber;
+
+//! Project version string for POC_Cocoapods.
+FOUNDATION_EXPORT const unsigned char POC_CocoapodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <POC_Cocoapods/PublicHeader.h>
+
+
---
+++ POC_Cocoapods/POC_Cocoapods/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  POC_Cocoapods
+//
+//  Created by global on 11/07/23.
+//
+
+import Foundation
+
+public class Service {
+
+    private init() {}
+
+    public func doSomething() -> String {
+        return "Do Something!"
+    }
+}
